be0cdd051107e4462f6e219e89b9c9af20b61174
android71Francis <francis.domanais.fernando@gmail.com>
2023-08-23 22:55:29 +0800

    [ReactBounty] Add About page content
    - Update about page with the content
    - Update the design for the main container to move in to layout component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -8,7 +8,9 @@ const Layout = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
-        <Outlet />
+        <div className="p-6 sm:py-10 flex flex-wrap items-center mx-auto max-w-screen-xl">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>This is the about page</h1>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-semibold mb-4">About Us</h1>
+      <p className="text-gray-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+        sagittis felis. Vivamus vel erat id urna congue ultricies. Aenean vitae
+        dolor in dolor tristique efficitur. Fusce volutpat ligula nec elementum
+        volutpat.
+      </p>
+      <p className="mt-4 text-gray-700">
+        Nullam a tortor dignissim, volutpat nulla a, faucibus quam. Integer
+        tincidunt diam eu orci pulvinar, sit amet bibendum dolor tristique.
+        Aliquam erat volutpat. Nam facilisis urna et aliquam tristique.
+      </p>
     </div>
   );
 };
