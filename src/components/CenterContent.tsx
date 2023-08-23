419a476110a9e7bdaaeac9ea11073f5e6807fada
android71Francis <francis.domanais.fernando@gmail.com>
2023-08-23 22:51:19 +0800

    [ReactBounty] Add center content in home
    - Add a center content in home page
    - Add new component center content

---
+++ src/components/CenterContent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CenterContent: React.FC = () => {
+  return (
+    <div className="bg-gray-800 flex flex-col items-center p-16 space-y-10 text-white">
+      <h4 className="text-4xl font-bold mb-4">What we do</h4>
+      <p className="mt-3 text-xl text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+    </div>
+  );
+};
+
+export default CenterContent;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import CenterContent from "../components/CenterContent";
 import HeroCarousel from "../components/HeroCarousel";
 
 const Home: React.FC = () => {
   return (
     <div className="p-6 sm:py-10 flex flex-wrap items-center mx-auto max-w-screen-xl">
       <HeroCarousel />
+      <CenterContent />
     </div>
   );
 };
